da249cafbcb621317dbf7eb906d4e9695bf7981f
Vlad Alive <vladalive@gmail.com>
2010-10-30 22:33:58 +0400

    [gem] fixes;

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
 # Include hook code here
 require 'active_navigation'
+ActiveRecord::Base.send :include, ActiveNavigation::Helpers::ClassMethods
+ActionView::Base.send :include, ActiveNavigation::Helpers::ClassMethods
 
--- lib/active_navigation.rb
+++ lib/active_navigation.rb
@@ -1,7 +1,3 @@
 require 'active_navigation/helpers'
 require 'active_navigation/nav_menu_class'
 
-class ActionView::Base
-  include ActiveNavigation::Helpers::ClassMethods
-end
-
