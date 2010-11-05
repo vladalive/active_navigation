231affb833bfbe3bbeb9da7d44c5c93c683cdbdd
Vlad Alive <vladalive@gmail.com>
2010-11-05 16:42:32 +0300

    [fix] views;

--- lib/active_navigation.rb
+++ lib/active_navigation.rb
@@ -5,3 +5,7 @@ require 'active_navigation/nav_menu_class'
 
 ActionView::Base.send :include, ActiveNavigation::Helpers
 
+ActionController::Base.class_eval do
+  append_view_path File.join(File.dirname(__FILE__), '..', 'app', 'views')
+end
+
